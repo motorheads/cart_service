f2138a43e651f9f4472530601310755245fc2704
RArdent <jahongirswe@gmail.com>
2022-07-12 02:34:44 +0500

    Update AddProduct feature

--- controller/product.go
+++ controller/product.go
@@ -1,14 +1,20 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/motorheads/cart_service/models"
+	"github.com/motorheads/cart_service/models"
 	// "github.com/motorheads/cart_service/storage"
 )
 
 func AddProduct(c *gin.Context) {
+	var cart models.Cart
+
+	if err := c.ShouldBindJSON(&cart); err != nil {
+		fmt.Println("Error while binding JSON cart (add product) ")
+	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "ok",
